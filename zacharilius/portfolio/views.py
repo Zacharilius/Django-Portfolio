51e0b0b0d58ed74476bbb324c958ad62ea6383b0
Zachary Bensley <Machine@Zacharys-MacBook-Pro.local>
2015-10-24 20:22:52 -0700

    Base template for/$ added

--- zacharilius/portfolio/views.py
+++ zacharilius/portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    context_dict = {'boldmessage': 'I am bolded'}
+    return render(request, 'portfolio/index.html', context_dict)
--- zacharilius/zacharilius/urls.py
+++ zacharilius/zacharilius/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'zacharilius.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^projects/', include('projects.urls')),
+    url(r'^$', include('portfolio.urls')),
 )
